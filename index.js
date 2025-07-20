fc61a4ef40aedb94dd391d4686e7d2649762f63e
Rama Seshu Vinnakota <84709698+ramaseshuvinnakota@users.noreply.github.com>
2025-07-20 15:06:26 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var tab = document.getElementsByClassName("nav-link");
-
-for (var i = 0; i < tab.length; i++ ) {
-  tab[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
